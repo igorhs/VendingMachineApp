2b44cf960ca3fc29280e0f123f53c88b2a10e6fe
Igor Henrique <igor_henrique8@hotmail.com>
2016-03-04 22:42:01 -0300

    Update: Commentaries

--- src/vendingmachine_igor/Products.java
+++ src/vendingmachine_igor/Products.java
@@ -5,11 +5,18 @@ package vendingmachine_igor;
  *
  * @author Igor Henrique da Silva
  */
+ 
+//Commentaries were set up both in English and Portuguese.
+
+//Criando a classe "Products".
+//Creating the class "Products".
 public class Products {
     private String name;
     private double price;
     private int quantity;
 
+    //Gerando getters e setters para os atributos "name", "price" e "quantity".
+    //Generating getters and setters for the "name", "price", and "quantity" attributes.
     public Products(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
@@ -39,5 +46,4 @@ public class Products {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-  
 }
