4d954d26514c5dc6c5e3e332a2a837a9bbb6a6f9
Igor Henrique <igor_henrique8@hotmail.com>
2016-03-04 23:14:23 -0300

    Update: Commentaries

--- src/vendingmachine_igor/VendingMachine_Igor.java
+++ src/vendingmachine_igor/VendingMachine_Igor.java
@@ -4,6 +4,9 @@ package vendingmachine_igor;
  *
  * @author Igor Henrique da Silva
  */
+ 
+//Commentaries were set up both in English and Portuguese.
+ 
 public class VendingMachine_Igor {
 
     //Criando classe para exibir a tela(ambiente) da Máquina de Vendas(Vending Machine).
