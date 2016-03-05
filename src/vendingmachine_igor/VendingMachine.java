3206f05f1b608eb3461aebd421178bd7e0a9a7c7
Igor Henrique <igor_henrique8@hotmail.com>
2016-03-04 22:53:58 -0300

    Update: Commentaries

--- src/vendingmachine_igor/VendingMachine.java
+++ src/vendingmachine_igor/VendingMachine.java
@@ -6,10 +6,19 @@ import java.util.ArrayList;
  *
  * @author Igor Henrique da Silva
  */
+ 
+//Commentaries were set up both in English and Portuguese.
+
+//Criando a classe "VendingMachine".
+//Creating the class "VendingMachine"
 public class VendingMachine {
     
+    //Criando uma ArrayList "p" do tipo "Products".
+    //Creating an ArrayList "p" from "Products".
     public static ArrayList<Products> p = new ArrayList<>(5);
     
+    //ArrayList criada para reestocar os produtos.
+    //Creating an ArrayList to restock products.
     public ArrayList restockProducts(){
         p.add(new Products("Coca-Cola", 1.5, 4));
         p.add(new Products("Fanta", 1, 3));
@@ -18,5 +27,4 @@ public class VendingMachine {
         p.add(new Products("Lays", 2, 4));
         return p;
     }
-    
 }
